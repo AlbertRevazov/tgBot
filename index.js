5a0d6532b876f05356aa2225ce1c7bdb112bd4b7
Albert <abeiphone@bk.ru>
2023-10-11 22:30:00 +0300

    draft

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const Telegram = require("node-telegram-bot-api");
 const commands = require("./data");
 const token = process.env.token;
 
-export TELEGRAM_API_TOKEN=... # YOUR TELEGRAM API TOKEN
-export TELEGRAM_WEBHOOK_URL=... # YOUR CYCLIC DEPLOYMENT URL
-
-curl "https://api.telegram.org/bot$TELEGRAM_API_TOKEN/setWebhook?url=$TELEGRAM_WEBHOOK_URL"
-
 const bot = new Telegram(token, { polling: true });
 bot.setMyCommands(commands);
 
